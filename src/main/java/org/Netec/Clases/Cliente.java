74b90745476091639afe1f9f4a35a4c0f1bbff80
defesamax@gmail.com <defesamax@gmail.com>
2024-09-24 10:37:27 -0500

    Actualizacion con patrón Builder

--- src/main/java/org/Netec/Clases/Cliente.java
+++ src/main/java/org/Netec/Clases/Cliente.java
@@ -13,17 +13,29 @@ public class Cliente implements ServicioCuentas, Comparable<Cuenta> {
     private Domicilio domicilio;
     private String rfc;
     private String telefono;
-    private ArrayList<Cuenta> cuentas;
+    private String cuentas;
     private String fechaNacimiento;
 
-    public Cliente(ClienteBuilder builder) {
+    public Cliente(int numero, String nombre, Domicilio domicilio, String rfc, String telefono, String cuentas, String fechaNacimiento) {
         this.numero = numero;
         this.nombre = nombre;
         this.domicilio = domicilio;
         this.rfc = rfc;
         this.telefono = telefono;
+        this.cuentas = cuentas;
         this.fechaNacimiento = fechaNacimiento;
     }
+//    public Cliente(ClienteBuilder builder) {
+//        this.numero = numero;
+//        this.nombre = nombre;
+//        this.domicilio = domicilio;
+//        this.rfc = rfc;
+//        this.telefono = telefono;
+//        this.fechaNacimiento = fechaNacimiento;
+//    }
+
+//    public Cliente(int i, String deybi) {
+//    }
 
     public int getNumero() {
         return numero;
@@ -65,12 +77,12 @@ public class Cliente implements ServicioCuentas, Comparable<Cuenta> {
         this.telefono = telefono;
     }
 
-    public ArrayList<Cuenta> getCuentas() {
+    public String getCuentas() {
         return cuentas;
     }
 
     public void setCuentas(ArrayList<Cuenta> cuentas) {
-        this.cuentas = cuentas;
+        this.cuentas = String.valueOf(cuentas);
     }
 
     public String getFechaNacimiento() {
@@ -92,6 +104,11 @@ public class Cliente implements ServicioCuentas, Comparable<Cuenta> {
                 ", cuentas=" + cuentas +
                 ", fechaNacimiento='" + fechaNacimiento + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Cuenta obj) {
+        return 0;
     }
 
     @Override
@@ -118,70 +135,97 @@ public class Cliente implements ServicioCuentas, Comparable<Cuenta> {
     public Cuenta obtenerCuentas() {
         return null;
     }
+//
+//    @Override
+//    public boolean agregarCuenta(Cuenta cuenta) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean cancelarCuenta(int numero) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void abonarCuenta(int Numero, double abono) {
+//
+//    }
+//
+//    @Override
+//    public void retirar(int numero, double retiro) {
+//
+//    }
+//
+//    @Override
+//    public Cuenta obtenerCuentas() {
+//        return null;
+//    }
+//
+//    @Override
+//    public int compareTo(Cuenta obj) {
+//        return 0;
+//    }
+//
+//    // Clase anidada estática Builder
+//    public static class ClienteBuilder {
+//        private int numero;
+//        private String nombre;
+//        private Domicilio domicilio;
+//        private String rfc;
+//        private String telefono;
+//        private ArrayList<Cuenta> cuentas = new ArrayList<>();
+//        private String fechaNacimiento;
+//
+//        // Constructor del Builder con los atributos obligatorios
+//        public ClienteBuilder(int numero, String nombre) {
+//            this.numero = numero;
+//            this.nombre = nombre;
+//        }
+//
+//
+//
+//        // Métodos para agregar los valores opcionales y retornando el mismo Builder (fluido)
+//        public ClienteBuilder conDomicilio(Domicilio domicilio) {
+//            this.domicilio = domicilio;
+//            return this;
+//        }
+//
+//        public ClienteBuilder conRfc(String rfc) {
+//            this.rfc = rfc;
+//            return this;
+//        }
+//
+//        public ClienteBuilder conTelefono(String telefono) {
+//            this.telefono = telefono;
+//            return this;
+//        }
+//
+//        public ClienteBuilder conCuentas(ArrayList<Cuenta> cuentas) {
+//            this.cuentas = cuentas;
+//            return this;
+//        }
+//
+//        public ClienteBuilder conFechaNacimiento(String fechaNacimiento) {
+//            this.fechaNacimiento = fechaNacimiento;
+//            return this;
+//        }
+//
+//        // Método para construir el objeto Cliente
+//        public Cliente build() {
+//            return new Cliente(this);
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "ClienteBuilder{" +
+//                    "numero=" + numero +
+//                    ", nombre='" + nombre + '\'' +
+//                    ", domicilio=" + domicilio +
+//                    ", rfc='" + rfc + '\'' +
+//                    ", telefono='" + telefono + '\'' +
+//                    ", cuentas=" + cuentas +
+//                    ", fechaNacimiento='" + fechaNacimiento + '\'' +
+//                    '}';
+//        }
 
-    @Override
-    public int compareTo(Cuenta obj) {
-        return 0;
-    }
-
-    // Clase anidada estática Builder
-    public static class ClienteBuilder {
-        private int numero;
-        private String nombre;
-        private Domicilio domicilio;
-        private String rfc;
-        private String telefono;
-        private ArrayList<Cuenta> cuentas = new ArrayList<>();
-        private String fechaNacimiento;
-
-        // Constructor del Builder con los atributos obligatorios
-        public ClienteBuilder(int numero, String nombre) {
-            this.numero = numero;
-            this.nombre = nombre;
-        }
-
-        // Métodos para agregar los valores opcionales y retornando el mismo Builder (fluido)
-        public ClienteBuilder conDomicilio(Domicilio domicilio) {
-            this.domicilio = domicilio;
-            return this;
-        }
-
-        public ClienteBuilder conRfc(String rfc) {
-            this.rfc = rfc;
-            return this;
-        }
-
-        public ClienteBuilder conTelefono(String telefono) {
-            this.telefono = telefono;
-            return this;
-        }
-
-        public ClienteBuilder conCuentas(ArrayList<Cuenta> cuentas) {
-            this.cuentas = cuentas;
-            return this;
-        }
-
-        public ClienteBuilder conFechaNacimiento(String fechaNacimiento) {
-            this.fechaNacimiento = fechaNacimiento;
-            return this;
-        }
-
-        // Método para construir el objeto Cliente
-        public Cliente build() {
-            return new Cliente(this);
-        }
-
-        @Override
-        public String toString() {
-            return "ClienteBuilder{" +
-                    "numero=" + numero +
-                    ", nombre='" + nombre + '\'' +
-                    ", domicilio=" + domicilio +
-                    ", rfc='" + rfc + '\'' +
-                    ", telefono='" + telefono + '\'' +
-                    ", cuentas=" + cuentas +
-                    ", fechaNacimiento='" + fechaNacimiento + '\'' +
-                    '}';
-        }
-    }
 }
--- src/main/java/org/Netec/Main.java
+++ src/main/java/org/Netec/Main.java
@@ -2,34 +2,89 @@ package org.Netec;
 
 import org.Netec.Clases.Banco;
 import org.Netec.Clases.Cliente;
+import org.Netec.Clases.Cuenta;
 import org.Netec.Clases.Domicilio;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
         Domicilio domicilio = new Domicilio("Calle 33", 311, "Guatape", "Antiquia", 5445);
         Banco banco = new Banco("Banco",domicilio, "qwerty", "4444444");
+        Cliente cliente = new Cliente(1, "Deybi", domicilio, "rfc123", "111 12345", "12345","2000-01-01");
 
-        // Crear un cliente usando el patrón Builder
-        Cliente cliente = new Cliente.ClienteBuilder(1, "Juan Pérez")
-                .conDomicilio(domicilio)
-                .conRfc("JUAP850101")
-                .conTelefono("555-1234")
-                .conFechaNacimiento("1985-01-01")
-                .build();
+//        // Crear un cliente usando el patrón Builder
+//        Cliente cliente = new Cliente.ClienteBuilder(1, "Juan Pérez")
+//                .conDomicilio(domicilio)
+//                .conRfc("JUAP850101")
+//                .conTelefono("555-1234")
+//                .conFechaNacimiento("1985-01-01")
+//                .build();
+//        Cliente cliente2 = new Cliente.ClienteBuilder(1, "Maria")
+//                .conDomicilio(domicilio)
+//                .conRfc("Call 2")
+//                .conTelefono("222-1234")
+//                .conFechaNacimiento("1985-01-01")
+//                .build();
+//        Cliente cliente3 = new Cliente.ClienteBuilder(1, "Jose")
+//                .conDomicilio(domicilio)
+//                .conRfc("Call 3")
+//                .conTelefono("333-1234")
+//                .conFechaNacimiento("1985-01-01")
+//                .build();
+//        Cliente cliente4 = new Cliente.ClienteBuilder(1, "David")
+//                .conDomicilio(domicilio)
+//                .conRfc("Call 4")
+//                .conTelefono("444-1234")
+//                .conFechaNacimiento("1985-01-01")
+//                .build();
+//        Cliente cliente5 = new Cliente.ClienteBuilder(1, "David")
+//                .conDomicilio(domicilio)
+//                .conRfc("Call 4")
+//                .conTelefono("444-1234")
+//                .conFechaNacimiento("1985-01-01")
+//                .build();
+        Cuenta cuenta1 = new Cuenta(1, "2024-01-01",120000, "") {
+            @Override
+            public double calcularIntereses() {
+                return 0;
+            }
+        };
+
+        Cuenta cuenta2 = new Cuenta(2, "2024-02-01",130000, "") {
+            @Override
+            public double calcularIntereses() {
+                return 0;
+            }
+        };
+
 
         // Imprimir el cliente
         System.out.println(cliente.toString());
+//        System.out.println(cliente2.toString());
+//        System.out.println(cliente3.toString());
+//        System.out.println(cliente4.toString());
+//        System.out.println(cliente5.toString());
+
         // Imprimir domicilio
         System.out.println(domicilio);
         //Imprimir Banco
         System.out.println(banco);
+        //Imprimir Cuenta
+        System.out.println(cuenta1);
+        System.out.println(cuenta2);
+
+        List<Cliente> listaclientes = new ArrayList<>();
+        listaclientes.add(new Cliente(6, "Deybi", domicilio, "rfc123","111 2222","123", "2000-01-01"));
+
+        listaclientes.stream()
+                .filter(t-> t.getNombre().startsWith("D"))
+                .forEach(t-> System.out.println(t));
 
     }
 
